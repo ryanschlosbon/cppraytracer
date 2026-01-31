f64a891c357ccc4a108b7d092743b9075427a73d
Ryan Schlosbon <rjs1214@live.com>
2026-01-31 02:13:40 -0500

    Added Camera FOV

--- build/main.cc
+++ build/main.cc
@@ -31,6 +31,9 @@ int main(){
     cam.max_depth = 50;
 
     cam.vfov = 70;
+    cam.lookfrom = point3(-2,2,1);
+    cam.lookat = point3(0,0,-1);
+    cam.vup = vec3(0,1,0);
 
     cam.render(world);
 }
